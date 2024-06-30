3f0e9192fe1be0446f820a4004c375ad96f438be
Namesakenberg <164608281+Namesakenberg@users.noreply.github.com>
2024-06-30 10:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (67.16%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,13 +1,8 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        //if we divide a number by 3 and get remainder 2 , 
-        //that number can never be expressed in terms of distinct powers of 3.
-        //for the numbers divisible by 3 . check by dividing the quotient
-        // example - 
-        // n = 15
-        // 15 % 3 is 0 , but 15 / 3 is 5 , and 5 % 3 is 2. hence false
+        //if the number has a remainder when divided by 3 then it is not the ans
         while(n>0){
-            if(n%3 == 2){
+            if(n%3 ==2 ){
                 return false;
             }
             n/=3;
