de0ae02d2b28d61aa0da0610961ae2975f62cd6e
Namesakenberg <164608281+Namesakenberg@users.noreply.github.com>
2024-07-01 23:08:17 +0530

    Time: 8 ms (49.66%) | Memory: 40 MB (65.80%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,8 @@
 class Solution {
     public int fib(int n) {
-        //recursion
-        if(n==1 || n==0){
+        if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        return fib(n-1) + fib(n-2);
     }
 }
