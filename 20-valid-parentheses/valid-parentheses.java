89a84066fd477615106f993ed82c1feaf0293286
Namesakenberg <164608281+Namesakenberg@users.noreply.github.com>
2024-07-12 18:15:45 +0530

    Time: 1 ms (98.97%) | Memory: 41.5 MB (38.51%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
-       Stack<Character> st = new Stack<>();
-        for(int i =0 ;i<s.length();i++){
+        Stack<Character>st =new Stack<>();
+        for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch == '(' || ch == '{' || ch == '['){
-              st.push(ch);
+            if(ch=='{'||ch=='['||ch=='('){
+                st.push(ch);
             }
-            else if (ch == ')' || ch =='}' || ch == ']'){
-                if(st.isEmpty()){
+            else if(ch=='}'){
+                if(st.isEmpty() || st.peek()!=('{')){
+                   return false;
+                }
+                  st.pop();
+            }
+             else if(ch==']'){
+                if(st.isEmpty() || st.peek()!=('[')){
+                   return false;
+                }
+                  st.pop();
+            }
+             else if(ch==')'){
+                if(st.isEmpty() || st.peek()!=('(')){
                     return false;
                 }
-                     char top= st.pop();
-                if((ch==')' && top != '(') 
-                || (ch=='}' && top != '{') 
-                || (ch==']' && top != '[')){
-                    return false;
-                }
+                  st.pop();
             }
-          
         }
-          return st.isEmpty();
+        return(st.isEmpty());
     }
-    }
+}
